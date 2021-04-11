37823afb6085c2920dafab3aa8914a1de25a8ead
smendescoti <sergio.coti@gmail.com>
2021-04-11 11:37:43 -0300

    Desenvolvimento de SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
+
+//importando a biblioteca de rotas do angular..
+import { Routes, RouterModule } from '@angular/router';
+
+//mapeamento das rotas para acesso aos componentes
+const routes:Routes = [
+  { path : 'cadastro-clientes', component : CadastroClientesComponent },
+  { path : 'consulta-clientes', component : ConsultaClientesComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClientesComponent,
+    ConsultaClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //registrando o mapeamento de rotas criado no projeto
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
